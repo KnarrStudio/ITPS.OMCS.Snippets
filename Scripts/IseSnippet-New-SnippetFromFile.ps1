fce700737bd858a6c5b14683fd6b98537effb43f
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-06-04 18:37:22 -0500

    Fixed the example

---
+++ Scripts/IseSnippet-New-SnippetFromFile.ps1
@@ -0,0 +1,14 @@
+﻿#requires -Version 3.0 -Modules ISE
+
+$SnipData = @{
+Title = 'ks_ New-Snippet from File' 
+Author = 'Erik' 
+Description = 'How to create a snippet from a file' 
+}
+
+$SnipText = @'
+(Get-Content -Path .\Snippet-ExampleFile.ps1)
+'@
+
+New-IseSnippet  @SnipData -Text (Get-Content -Path .\Snippet-ExampleFile.ps1 -Raw) -Force
+
---
+++ Scripts/Snippet-ExampleFile.ps1
@@ -0,0 +1,31 @@
+﻿$SnipData = @{
+Title = 'ks_ New-Snippet from File' 
+Author = 'Erik' 
+Description = 'How to create a snippet from a file' 
+}
+
+$SnipText = @'
+(Get-Content -Path .\Snippet-ExampleFile.ps1 -Raw)
+'@
+
+New-IseSnippet  @SnipData -Text $SnipText
+
+# OR
+
+New-IseSnippet -Title 'Snippet Name' -Author 'Erik' 
+-Description 'How to create a snippet from a file' 
+-Text (Get-Content .\SomeCodeSnippet.ps1 -Raw)
+
+# OR
+
+New-IseSnippet -Title 'Snippet Name' -Author 'Erik' 
+-Description 'How to create a snippet from a file' 
+-Text @'
+
+Get-Process powershell_ise
+Start-Process winword
+'@
+
+
+
+
