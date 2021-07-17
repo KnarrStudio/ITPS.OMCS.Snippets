8887a3ed7cda8d1256ee00ada769874353bbb9b3
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-07-16 23:08:44 -0500

    Update IseSnippet-LabelAndExpression.ps1

--- Scripts/IseSnippet-LabelAndExpression.ps1
+++ Scripts/IseSnippet-LabelAndExpression.ps1
@@ -1,24 +1,13 @@
-﻿
-
-$SnipData = @{
+﻿$SnipData = @{
 Title = 'ks: Label and Expression' 
 Author = 'Erik' 
 Description = 'Formatting output for more human kindness' 
 }
-
 $SnipText = @'
 @{ label = ""; expression = {} }
 
 Example: Get-ChildItem -File |
 Format-Wide -Property @{e={'{0} ({1:N2}kb)' -f $_.name,($_.length/1kb)}}
 '@
-
 New-IseSnippet  @SnipData -Text $SnipText
-
-
-
-
-
-
-
   
