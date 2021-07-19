0e4bb25dee312b7c0854ae3af23a3964a09b95ad
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-07-18 19:56:34 -0500

    Added new snippet and correct older one

---
+++ Scripts/IseSnippet-NestedHashTable.ps1
@@ -0,0 +1,22 @@
+﻿#requires -Version 2.0 -Modules ISE
+
+$SnipData = @{
+  Title       = 'ks: Hash Tables Nested'
+  Author      = 'Erik'
+  Description = 'Nested hash table example'
+}
+
+$SnipText = @'
+$MyVar = @{
+  tst1 = @{
+    tst1a = 'c:\temp\'
+    tst1b = 'Pumkin'
+  }
+  tet2 = 'testing'
+}
+
+'@
+
+New-IseSnippet  @SnipData -Text $SnipText
+
+
--- Scripts/IseSnippet-New-SnippetFromFile.ps1
+++ Scripts/IseSnippet-New-SnippetFromFile.ps1
@@ -1,16 +1,33 @@
 ﻿#requires -Version 3.0 -Modules ISE
+#Filename - IseSnippet-NestedHashTable.ps1
+
 
 $SnipData = @{
-Title = 'ks: New-Snippet from File' 
-Author = 'Erik' 
-Description = 'How to create a snippet from a file' 
+  Title       = 'ks: New-Snippet from File'
+  Author      = 'Erik'
+  Description = 'How to create a snippet from a file'
 }
 
-$SnipText = @'
-(Get-Content -Path .\Snippet-ExampleFile.ps1)
+$SnipText = Get-Content -Path .\Snippet-ExampleFile.ps1 -Raw
+
+New-IseSnippet  @SnipData -Text $SnipText -Force
+
+# OR
+
+New-IseSnippet -Title 'Snippet Name' -Author 'Erik' 
+-Description 'How to create a snippet from a file' 
+-Text (Get-Content .\SomeCodeSnippet.ps1 -Raw)
+
+# OR
+
+New-IseSnippet -Title 'Snippet Name' -Author 'Erik' 
+-Description 'How to create a snippet from a file' 
+-Text @'
+
+Get-Process powershell_ise
+Start-Process winword
 '@
 
-$mypath = Split-Path ($MyInvocation.MyCommand.Path) -Parent
 
-New-IseSnippet  @SnipData -Text (Get-Content -Path "$mypath\Snippet-ExampleFile.ps1" -Raw) -Force
+
 
