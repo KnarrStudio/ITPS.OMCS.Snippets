d4d9c4afb7c67c4ce23ec731f510bcbab34eeb43
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-08-10 08:53:13 -0500

    Filename corrections
    removal of "s" from name to make them the same, plus removed obsolete files

---
+++ Scripts/IseSnippet-New-Snippet_Examples.ps1
@@ -0,0 +1,15 @@
+﻿#requires -Version 3.0 -Modules ISE 
+
+# File with the content '.\Snippet-ExampleFile.txt'
+
+
+$SnipData = @{
+  Title       = 'ks: New-Snippet_Examples'
+  Author      = 'Erik'
+  Description = 'Examples of how to create a snippet'
+}
+
+$SnipText = Get-Content -Path .\Snippet-ExampleFile.txt -Raw
+
+New-IseSnippet  @SnipData -Text $SnipText -Force
+
