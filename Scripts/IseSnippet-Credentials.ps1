38d0f13faf3e9c7142c82d6ecb2c2a06df00e929
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-12-19 07:55:38 -0600

    Update IseSnippet-Credentials.ps1
    update for better reading

--- Scripts/IseSnippet-Credentials.ps1
+++ Scripts/IseSnippet-Credentials.ps1
@@ -1,11 +1,12 @@
 ﻿
 $m = (@'
+# Save Credentials
+$CredentialFile = '{0}\{1}.cred' -f $env:USERPROFILE, $env:USERNAME 
+Get-Credential | Export-CliXml -Path $CredentialFile
 
-$CredFile = '{0}\{1}.cred' -f $env:USERPROFILE, $env:USERNAME
-# $CredFile = '{0}.cred' -f $env:USERNAME
-$Credential = Get-Credential | Export-CliXml -Path $CredFile
-$Credential = Import-CliXml -Path $CredFile
-
+# Use Credentials
+$CredentialFile = '{0}\{1}.cred' -f $env:USERPROFILE, $env:USERNAME 
+$creds = Import-CliXml -Path $CredentialFile
 '@ )
 
 New-IseSnippet -Text $m -Title 'ks: Credential File' -Description 'Save credentials securely.' -Author 'Knarr Studio' -Force
